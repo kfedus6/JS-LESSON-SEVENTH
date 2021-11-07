e8c1fa51492df9bd09936842d7d7e127a7447f8c
kfedus6 <feduskolya@gmail.com>
2021-11-07 17:09:02 +0200

    Sample code

---
+++ index.js
@@ -0,0 +1,34 @@
+//============ ДЗ =================\\
+/*let arr = [5, 3, 8, 1];
+let filterRangeInPlace = (arr, a, b) => {
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < a || arr[i] > b) {
+         arr.splice(i, 1);
+         i--;
+      }
+   }
+}
+console.log(arr);
+filterRangeInPlace(arr, 1, 4);
+console.log(arr);
+*/
+/*
+function unique(arr) {
+   let result = [];
+
+   for (let str of arr) {
+      if (!result.includes(str)) {
+         result.push(str);
+      }
+   }
+
+   return result;
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+   "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+alert(unique(strings));
+*/
+
